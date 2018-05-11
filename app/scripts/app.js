33493a35791722a2fcc95661556ab351d761296b
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2018-05-11 13:52:00 -0400

    Wrap XHR in a Promise

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,23 +35,28 @@ Instructions:
   function get(url) {
     /*
     This code needs to get wrapped in a Promise!
-     */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
+    */
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = function() {
+        if (req.status === 200) {
+          // It worked!
+          // You'll want to resolve with the data from req.response
+          resolve(req.response);
+        } else {
+          // It failed :(
+          // Be nice and reject with req.statusText
+          reject(Error(req.statusText));
+        }
+      };
+      req.onerror = function() {
         // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+        // Pass a 'Network Error' to reject
+        reject(Error('Network Error'));
+      };
+      req.send();
+    });
   }
 
   window.addEventListener('WebComponentsReady', function() {
